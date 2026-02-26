5a357a02140ccb8f610d7860a16338e117584778
licorisy <klepehin.k.a@gmail.com>
2026-02-26 16:06:11 +0300

    Update PostmanEchoTest.java

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,7 +20,7 @@ class PostmanEchoTest{
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("hi"))
+                .body("data", equalTo("hello"))
         ;
 }
 }
